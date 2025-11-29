369964f9a241b2a0d205489ef0a85b2a9fb7143d
mikhail.zakharov <michael.zacharov@ottofeller.com>
2025-11-29 14:09:12 +0100

    Export utility functions and types from `src/index.ts`.

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { fetchNamFiles } from "./lib/fetchNam";
+export { convertNamToRanges } from "./lib/convertNamToRanges";
+export type { Subset, UnicodeRange } from "./lib/convertNamToRanges";
