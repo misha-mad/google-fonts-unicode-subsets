b6c21498dc96649b3a2b529b7015aa0e93a39994
mikhail.zakharov <michael.zacharov@ottofeller.com>
2025-11-29 15:32:52 +0100

    **Remove placeholder functions for `.nam` file handling; refactor exports and constants**

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const NAM_FILES_URL =
+  "https://raw.githubusercontent.com/googlefonts/nam-files/main/Lib/gfsubsets/data";
+
+export const GITHUB_API_URL =
+  "https://api.github.com/repos/googlefonts/nam-files/contents/Lib/gfsubsets/data";
