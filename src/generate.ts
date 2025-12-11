cf3343401e0ba4dfb19e231c9dec1d0426d11536
Vladimir Agafonov <agafvv@gmail.com>
2025-12-11 16:55:32 +0300

    Optimize code

--- src/generate.ts
+++ src/generate.ts
@@ -1,43 +1,28 @@
 import {writeFileSync} from 'fs'
 import {resolve} from 'path'
-import {FontSubsets, getSubsetList, parseNamFile, readNamFile, toConstName, toDisplayName} from './lib'
+
+import {
+  FontSubsets,
+  getSubsetList,
+  parseNamFile,
+  POPULARITY_ORDER,
+  readNamFile,
+  toConstName,
+  toDisplayName,
+} from './lib'
 
 async function generateJson() {
   console.log('🚀 Starting subset generation (JSON)...\n')
   const subsets = await getSubsetList()
 
-  const POPULARITY_ORDER = [
-    'latin_unique-glyphs',
-    'latin-ext_unique-glyphs',
-    'cyrillic_unique-glyphs',
-    'cyrillic-ext_unique-glyphs',
-    'greek_unique-glyphs',
-    'greek-ext_unique-glyphs',
-    'vietnamese_unique-glyphs',
-    'chinese-simplified_unique-glyphs',
-    'chinese-traditional_unique-glyphs',
-    'chinese-hongkong_unique-glyphs',
-    'japanese_unique-glyphs',
-    'korean_unique-glyphs',
-    'arabic_unique-glyphs',
-    'hebrew_unique-glyphs',
-    'devanagari_unique-glyphs',
-    'thai_unique-glyphs',
-    'math_unique-glyphs',
-  ]
-
-  subsets.sort((a, b) => {
-    const indexA = POPULARITY_ORDER.indexOf(a)
-    const indexB = POPULARITY_ORDER.indexOf(b)
-
-    if (indexA !== -1 && indexB !== -1) {
-      return indexA - indexB
-    }
-
-    if (indexA !== -1) return -1
-    if (indexB !== -1) return 1
-    return a.localeCompare(b)
-  })
+  // Sort sets according to POPULARITY_ORDER.
+  subsets.sort(
+    ((orderMap) => (a, b) => {
+      const indexA = orderMap[a] ?? Infinity
+      const indexB = orderMap[b] ?? Infinity
+      return indexA === indexB ? a.localeCompare(b) : indexA - indexB
+    })(Object.fromEntries(POPULARITY_ORDER.map((name, i) => [name, i]))),
+  )
 
   const dataUnicodeNotation: FontSubsets = {}
   let totalCodepoints = 0
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,23 @@
 import {resolve} from 'path'
 
 export const NAM_FILES_ROOT = resolve(process.cwd(), 'nam-files/Lib/gfsubsets/data')
+
+export const POPULARITY_ORDER = [
+  'latin_unique-glyphs',
+  'latin-ext_unique-glyphs',
+  'cyrillic_unique-glyphs',
+  'cyrillic-ext_unique-glyphs',
+  'greek_unique-glyphs',
+  'greek-ext_unique-glyphs',
+  'vietnamese_unique-glyphs',
+  'chinese-simplified_unique-glyphs',
+  'chinese-traditional_unique-glyphs',
+  'chinese-hongkong_unique-glyphs',
+  'japanese_unique-glyphs',
+  'korean_unique-glyphs',
+  'arabic_unique-glyphs',
+  'hebrew_unique-glyphs',
+  'devanagari_unique-glyphs',
+  'thai_unique-glyphs',
+  'math_unique-glyphs',
+]
